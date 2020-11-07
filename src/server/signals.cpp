a70b4701652f0f0479c7810f3fd05d279d96095a
skuppers <sebastien.kuppers@gmail.com>
2020-11-07 18:29:56 +0100

    Removed useless sighandlers

--- src/server/signals.cpp
+++ src/server/signals.cpp
@@ -18,27 +18,11 @@ void catch_secondary_signal(int sig)
 	g_signalTerminate = true;
 }
 
-void catch_tertiary_signal(int sig)
-{
-	std::string signame = strsignal(sig);
-	g_reporter->log(LOGLVL_ERROR, "Internal error signal catched (" + signame + "). Quitting!");
-	g_signalTerminate = true;
-}
-
 void init_signal_handler(void)
 {
 	if (SIG_ERR == signal(SIGHUP, 	  catch_primary_signal)
 		|| SIG_ERR == signal(SIGINT,  catch_primary_signal)
 		|| SIG_ERR == signal(SIGQUIT, catch_secondary_signal)
-  		|| SIG_ERR == signal(SIGILL,  catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGTRAP, catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGABRT, catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGIOT,  catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGFPE,  catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGBUS,  catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGSEGV, catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGSYS,  catch_tertiary_signal)
-  		|| SIG_ERR == signal(SIGPIPE, catch_tertiary_signal)
   		|| SIG_ERR == signal(SIGALRM, catch_primary_signal)
   		|| SIG_ERR == signal(SIGTERM, catch_secondary_signal)
   		|| SIG_ERR == signal(SIGURG,  catch_primary_signal)
